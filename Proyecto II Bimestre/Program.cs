cde4d423071f353f237dfc529886484c42060feb
Giuliana2106 <62736506+Giuliana2106@users.noreply.github.com>
2020-04-17 22:39:35 -0600

    Proyecto

---
+++ Proyecto II Bimestre/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Proyecto_II_Bimestre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double monto;
+            double x;
+            double descuento;
+            double total;
+            string linea;
+
+            Console.WriteLine("Ingrese el total a pagar: ");
+            linea = Console.ReadLine();
+            monto = double.Parse(linea);
+
+            if (monto > 1500) ;
+            {
+                if (monto > 2000) ;
+                {
+                    if (monto > 3000) ;
+                    {
+                        x = 0.25;
+                        descuento = monto * x;
+                        total = monto - descuento;
+                        Console.WriteLine("El total a pagar es: +total");
+
+                    }
+
+                 else
+                    {
+                        x = 0.10;
+                        descuento = monto * x;
+                        total = monto - descuento;
+                        Console.WriteLine("El total a pagar es: +total");
+                    }
+                }
+
+                else
+                {
+                    x = 0.10;
+                    descuento = monto * x;
+                    total = monto - descuento;
+                    Console.WriteLine("El total a pagar es: +total");
+                }
+                else
+                {
+                    Console.WriteLine("El total a pagar es: +monto");
+
+                }
+                Console.ReadKey();
+            }
+
+        
+    }
+}
